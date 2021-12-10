47315e206116d65dadc676d553b39d086c0f2199
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-10 21:48:17 +0200

    test element ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>Nikolis</h1>
+  <h1>
+    <div class="block">
+      <span class="demonstration">Default value</span>
+      <el-slider v-model="value1"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">Customized initial value</span>
+      <el-slider v-model="value2"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">Hide Tooltip</span>
+      <el-slider v-model="value3" :show-tooltip="false"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">Format Tooltip</span>
+      <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
+    </div>
+    <div class="block">
+      <span class="demonstration">Disabled</span>
+      <el-slider v-model="value5" disabled></el-slider>
+    </div>
+  </h1>
 </template>
 
 <script>
@@ -9,12 +30,21 @@ export default {
     msg: String,
   },
   data() {
-    return {};
+    return {
+      value1: 0,
+      value2: 50,
+      value3: 36,
+      value4: 48,
+      value5: 42,
+    };
   },
   methods: {
     test() {
       console.log("test");
       return "fsafsadasa";
+    },
+    formatTooltip(val) {
+      return val / 100;
     },
   },
 };
