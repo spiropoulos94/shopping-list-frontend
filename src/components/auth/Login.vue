b0e5985993f7d2ff218ace7149ac7810de41f042
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-13 20:45:37 +0200

    added login and signup page

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Login</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>SignUp</div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
