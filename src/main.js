ab40b38c00cec9af19d2ecde59893d2aad9d5b15
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-13 20:51:53 +0200

    installed vuex and vue router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
+import Vuex from "vuex";
+import "es6-promise/auto";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(Vuex);
 
 new Vue({
   render: (h) => h(App),
