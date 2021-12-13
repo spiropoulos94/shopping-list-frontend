f2fe910b973f28604d16890f22c24ee77b5478e8
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-13 21:01:06 +0200

    nothing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" style="margin: auto; max-width: 1200px; padding: 30px">
+  <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  margin: auto;
+  max-width: 1200px;
+  padding: 30px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,5 @@
 <template>
-  <div>
-    <div class="block">
-      <span class="demonstration">Default value</span>
-      <el-slider v-model="value1"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">Customized initial value</span>
-      <el-slider v-model="value2"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">Hide Tooltip</span>
-      <el-slider v-model="value3" :show-tooltip="false"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">Format Tooltip</span>
-      <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demonstration">Disabled</span>
-      <el-slider v-model="value5" disabled></el-slider>
-    </div>
-  </div>
+  <div>Hello World</div>
 </template>
 
 <script>
