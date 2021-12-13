7ba2080c82d90d05ad11e205512cd8926b837dd5
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-13 23:47:30 +0200

    if you are logged in and type non existing, you get not found. if you are not logged in, you are constantly redirected to login

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Not found</h1>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*  */
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Login from "@/views/auth/Login";
 import Signup from "@/views/auth/Signup";
 import Home from "@/views/Home";
+import NotFound from "@/components/NotFound";
 
 Vue.use(VueRouter);
 
@@ -38,6 +39,17 @@ const routes = [
     path: "/signup",
     name: "signup",
     component: Signup,
+  },
+  {
+    path: "*",
+    component: NotFound,
+    beforeEnter(to, from, next) {
+      if (hasUser) {
+        next();
+      } else {
+        next("/");
+      }
+    },
   },
 ];
 
